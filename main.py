a87ce49ab9f21394dad5c0bd152850923edc1c61
zhike <54851870+913926816@users.noreply.github.com>
2025-02-27 13:39:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
+from astrbot.api import logger
 
-@register("astrbot_plugin_QQbotbd", "英一智", "一个扫码登录绑定QQ号的插件", "1.0.0", "https://github.com/913926816/astrbot_plugin_QQbotbd/")
+@register("astrbot_plugin_QQbotbd", "英一智", "我是一个简单的QQ扫码登录插件", "1.0.0", "https://github.com/913926816/astrbot_plugin_QQbotbd")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -9,7 +10,9 @@ class MyPlugin(Star):
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("helloworld")
     async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
+        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
-        message_str = event.message_str # 获取消息的纯文本内容
-        yield event.plain_result(f"Hello, {user_name}!") # 发送一条纯文本消息
+        message_str = event.message_str # 用户发的纯文本消息字符串
+        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+        logger.info(message_chain)
+        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
